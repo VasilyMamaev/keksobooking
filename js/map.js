bfdb002ac7b41191b4773bd543ed37c8bf6ca820
VasilyMamaev <mamaev93@mail.ru>
2019-11-30 21:49:53 +0500

    отрисовал объявления на блоке .map

--- js/map.js
+++ js/map.js
@@ -1,12 +1,77 @@
 'use strict';
 
 (function () {
-  
- // удаление класса
 
- var map = document.querySelector('.map');
- map.classList.remove('map--faded');
+    // удаление класса
 
- console.log(document.mockAds)
+    var map = document.querySelector('.map');
+    map.classList.remove('map--faded');
+
+
+    //  содание шаблонов
+
+    var pinsMap = document.querySelector('.map__pins');
+    var parentFragment = document.createDocumentFragment();
+    var similarAdTemplate = document.querySelector('#temp')
+        .content.querySelector('.map__card');
+
+    for (var i = 0; i < document.mockAds.length; i++) {
+        var similarAd = similarAdTemplate.cloneNode(true);
+    
+    // функция отрнисовки features
+        var createFeatures = function () {
+            var featuresArr = document.mockAds[i].offer.features;
+            for (var k = 0; k < featuresArr.length; k++) {
+                var el = document.createElement('li');
+                el.className = `popup__feature popup__feature--${featuresArr[k]}`;
+                similarAd.querySelector('.popup__features').append(el);
+            }
+        };
+    // функция отрисовки photos
+        var createPhotos = function () {
+            var photosArr = document.mockAds[i].offer.photos;
+            
+            for (var j = 0; j < photosArr.length; j++) {
+                var elem = similarAd.querySelector('.popup__photo');
+                var photo = elem.cloneNode(true);
+                photo.src = `${photosArr[j]}`;
+                similarAd.querySelector('.popup__photos').appendChild(photo)
+            }
+
+        };
+
+    // заполнение шаблонов
+
+        similarAd.style = `left: ${document.mockAds[i].location.x}px; top: ${document.mockAds[i].location.y}px`
+        similarAd.querySelector('.popup__avatar').src = document.mockAds[i].author.avatar;
+        similarAd.querySelector('.popup__title').textContent = document.mockAds[i].offer.title;
+        similarAd.querySelector('.popup__text--address').textContent = document.mockAds[i].offer.address;
+        similarAd.querySelector('.popup__text--price').textContent = document.mockAds[i].offer.price;
+        similarAd.querySelector('.popup__type').textContent = document.mockAds[i].offer.type;
+        similarAd.querySelector('.popup__text--capacity').textContent = `${document.mockAds[i].offer.rooms} комнаты на ${document.mockAds[i].offer.guests} гостей`;
+        similarAd.querySelector('.popup__text--time').textContent = `Заезд после ${document.mockAds[i].offer.checkin}, выезд до ${document.mockAds[i].offer.checkout}`;
+        similarAd.querySelector('.popup__features').innerHTML = '';
+        createFeatures();
+        similarAd.querySelector('.popup__description').textContent = document.mockAds[i].offer.description;
+        createPhotos();
+
+        parentFragment.appendChild(similarAd);
+    }
+
+    pinsMap.appendChild(parentFragment);
+
+    // удаление пустого img
+    document.addEventListener("DOMContentLoaded", () => {
+        var list = document.querySelectorAll('.popup__photos');
+        console.log(list)
+        list.forEach((listEl, idx) => {
+            console.log(listEl.childNodes[1])
+            listEl.removeChild(listEl.childNodes[1])
+        })
+    });
+        
+    
+    
+        
 
 })();
